9b850b2284f43d7f0b1929928c2a8800b2340ec3
Patrick <shiawasearuji@gmail.com>
2025-11-20 14:45:54 +0800

    taliwind init

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title>@yield('title', 'My Portfolio')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    @vite('resources/css/app.css')
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-white">
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
